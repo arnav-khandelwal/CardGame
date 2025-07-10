50f8d9dee7953cfd0b21479554e7e0f2446bec98
Arnav Khandelwal <arnav181104@gmail.com>
2025-07-10 10:11:51 +0530

    basic UI

--- warcardgame/ContentView.swift
+++ warcardgame/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Spacer()
+            Image("logo")
+            Spacer()
+            HStack{
+                Spacer()
+                Image("card2")
+                Spacer()
+                Image("card3")
+                Spacer()
+            }
+            Spacer()
+            Image("button")
+            Spacer()
+            HStack {
+                Spacer()
+                VStack{
+                    Text("Player")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text("0")
+                        .font(.largeTitle)
+                }
+                Spacer()
+                VStack{
+                    Text("Computer")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text("0")
+                        .font(.largeTitle)
+                }
+                Spacer()
+            }.foregroundStyle(Color(.white))
+            Spacer()
+        }.background(Image("background-plain"))
     }
 }
 
