68a8bcd3a714a6ce994d98ce75edf78047bc12c8
Arnav Khandelwal <arnav181104@gmail.com>
2025-07-10 21:28:08 +0530

    Final product ready

--- warcardgame/ContentView.swift
+++ warcardgame/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var PlayerCard = "card3"
+    @State var ComputerCard = "card2"
+    @State var PlayerScore = 0
+    @State var ComputerScore = 0
+    
     var body: some View {
-        
-        var PlayerCard = "card3"
-        var ComputerCard = "card2"
-        var PlayerScore = 0
-        var ComputerScore = 0
-        
         
         VStack {
             Spacer()
@@ -54,10 +54,22 @@ struct ContentView: View {
                 Spacer()
             }.foregroundStyle(Color(.white))
             Spacer()
-        }.background(Image("background-plain"))
+        }.background(Image("background-plain")
+                             .resizable()
+                             .ignoresSafeArea())
     }
     func deal(){
-        print("Deal")
+        let PlayerCardValue = Int.random(in: 2...14)
+        let ComputerCardValue = Int.random(in: 2...14)
+        PlayerCard = "card" + String(PlayerCardValue)
+        ComputerCard = "card" + String(ComputerCardValue)
+        
+        if PlayerCardValue > ComputerCardValue{
+            PlayerScore += 1
+        }else if PlayerCardValue < ComputerCardValue{
+            ComputerScore += 1
+        }
+        
     }
 }
 
